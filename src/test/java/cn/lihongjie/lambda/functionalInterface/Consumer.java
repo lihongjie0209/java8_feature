e394f09237f807319f635bf555da1791ac007ae4
lihongjie <lihongjie0209@gmail.com>
2018-11-28 21:39:28 +0800

    添加: 消费者接口基本实现以及测试

---
+++ src/test/java/cn/lihongjie/lambda/functionalInterface/Consumer.java
@@ -0,0 +1,17 @@
+package cn.lihongjie.lambda.functionalInterface;
+
+/**
+ * @author 982264618@qq.com
+ */
+@FunctionalInterface
+public interface Consumer<T> {
+
+
+	void cosume(T o);
+
+
+
+
+
+
+}
---
+++ src/test/java/cn/lihongjie/lambda/functionalInterface/ConsumerTest.java
@@ -0,0 +1,62 @@
+package cn.lihongjie.lambda.functionalInterface;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author 982264618@qq.com
+ */
+public class ConsumerTest {
+
+	private static Logger logger = getLogger(ConsumerTest.class);
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+
+	@Test
+	public void testConsumer() throws Exception {
+
+
+		Consumer<String> stringConsumer = new Consumer<String>() {
+			@Override
+			public void cosume(String o) {
+				logger.info(o);
+			}
+		};
+		stringConsumer.cosume("hello world");
+
+	}
+
+	@Test
+	public void testConsumerWithLambda() throws Exception {
+
+
+		Consumer<String> stringConsumer = o -> logger.info(o);
+		stringConsumer.cosume("hello world");
+
+	}
+
+	@Test
+	public void testConsumerWithMethodRefer() throws Exception {
+
+
+		Consumer<String> stringConsumer = logger::info;
+		stringConsumer.cosume("hello world");
+
+	}
+
+
+
+
+
+}
